66db1e3a002f2c41be6e2583c3f1a4d3cebfd2e6
Jonathan Bolduc <bolduc.jonathan@hotmail.com>
2018-01-22 20:17:07 -0500

    fin de la 2e section

--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -1,8 +1,7 @@
 import { Component, OnInit } from '@angular/core';
-import { setTimeout } from 'timers';
 
 @Component({
-  // selector: '[app-servers]', 
+  // selector: '[app-servers]',
   // selector: '.app-servers',
   selector: 'app-servers',
   templateUrl: './servers.component.html',
@@ -12,10 +11,13 @@ export class ServersComponent implements OnInit {
   allowNewServer = false;
   serverCreationStatus = 'No server was created';
   serverName = 'Testserver';
-  constructor() { 
+  serverCreated = false;
+  servers = ['testserver', 'testserver 2'];
+
+  constructor() {
     setTimeout(() => {
       this.allowNewServer = true;
-    },2000);
+    }, 2000);
 
   }
 
@@ -23,7 +25,9 @@ export class ServersComponent implements OnInit {
   }
 
   onCreateServer() {
-    this.serverCreationStatus = 'Server was created!';
+    this.serverCreated = true;
+    this.servers.push(this.serverName);
+    this.serverCreationStatus = 'Server was created!  Name is ' + this.serverName;
   }
 
   onUpdateServerName(event: any){
